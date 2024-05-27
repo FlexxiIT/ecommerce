d6182719cbabdafdc61dd859663efc4024623b01
Lucafu49 <lucasmena1403@gmail.com>
2024-05-27 20:15:37 -0300

    feat: (postalCode api included)

---
+++ frontend/src/app/services/geocoding.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GeocodingService {
+
+  private apiUrl = `http://api.zippopotam.us/AR/`
+
+  constructor(private http: HttpClient) {}
+
+  getLocation(postalCode : string){
+    return this.http.get(`${this.apiUrl}${postalCode}`)
+  }
+}
